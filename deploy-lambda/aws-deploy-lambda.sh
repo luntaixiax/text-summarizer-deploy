13f59d76da414db9ba6a7264b789c8491d8c1262
luntaixia <ailunqian124@gmail.com>
2023-06-21 23:24:22 -0400

    docker build test for lambda

--- deploy-lambda/aws-deploy-lambda.sh
+++ deploy-lambda/aws-deploy-lambda.sh
@@ -1,6 +1,7 @@
 AWS_REGION=ca-central-1
 AWS_ACCT_ID=981401473042
 REPO_NAME="cnn-summarizer"
+LAMBDA_NAME="cnn-summarizer"
 DOCKER_IMAGE_NAME="luntaixia/cnn-summarizer-lambda"
 ROLE_NAME="lambda-ex"  # TODO: update this
 
@@ -9,13 +10,13 @@ ECR_REPO_URI="$AWS_ACCT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME"
 
 # create role before move on
 aws lambda create-function \
-  --function-name $REPO_NAME \  # lambda function name
+  --function-name $LAMBDA_NAME \  # lambda function name
   --package-type Image \  # the type of lambda function
   --code ImageUri=$ECR_REPO_URI:latest \  # ECR image URI
   --role arn:aws:iam::$AWS_ACCT_ID:role/$ROLE_NAME # IAM ARN number
 
 # update lambda if updated image
 aws lambda update-function-code \
-	--function-name $REPO_NAME  \
+	--function-name $LAMBDA_NAME  \
 	--region $AWS_REGION \
 	--image-uri $ECR_REPO_URI:latest
