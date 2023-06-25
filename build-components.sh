c65cd6753528c8237f4be697eaa144732d7f1c79
luntaixia <ailunqian124@gmail.com>
2023-06-24 23:30:34 -0400

    fix build

--- build-components.sh
+++ build-components.sh
@@ -1,5 +1,6 @@
-docker build -t luntaixia/cnn-summarizer-mlflow /deploy-mlflow;
-docker build -t luntaixia/cnn-summarizer-backend /backend-fastapi;
-docker build -t luntaixia/cnn-summarizer-local /deploy-fastapi;
-docker build -t luntaixia/cnn-summarizer-monitoring /monitoring;
-docker build -t luntaixia/cnn-summarizer-frontend /frontend-streamlit;
+#!/bin/sh
+docker build -t luntaixia/cnn-summarizer-mlflow ./deploy-mlflow;
+docker build -t luntaixia/cnn-summarizer-backend ./backend-fastapi;
+docker build -t luntaixia/cnn-summarizer-local ./deploy-fastapi;
+docker build -t luntaixia/cnn-summarizer-monitoring ./monitoring;
+docker build -t luntaixia/cnn-summarizer-frontend ./frontend-streamlit;
