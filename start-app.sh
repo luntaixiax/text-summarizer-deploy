30cfae1f64c78c4873e82419fb89411ee4626993
luntaixia <ailunqian124@gmail.com>
2023-06-25 00:40:38 -0400

    fix dashboard bug

--- start-app.sh
+++ start-app.sh
@@ -1,6 +1,5 @@
 export VOLUME_MAPPING_PATH=/home/luntaixia/Downloads;
-mkdir -p volume-mapping;
-export VOLUME_MAPPING_PATH=${PWD}/volume-mapping;
+#mkdir -p volume-mapping;
+#export VOLUME_MAPPING_PATH=${PWD}/volume-mapping;
 
-chmod -x start-app.sh;
 docker-compose up;
